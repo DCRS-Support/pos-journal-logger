028b0d3a971ef9497f319872f9776984c47ec684
Caleb Brendel <109250665+CalebBrendel@users.noreply.github.com>
2025-05-08 11:43:51 -0500

    Update windows.ps1

--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -8,7 +8,7 @@ mkdir C:\pos-journal-logger\scripts
 
 REM Installing Chocolately if not installed...
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+# @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM Installing curl if not installed...
 
