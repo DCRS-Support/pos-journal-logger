ffaec0874f3dde5d758b84d396103e66f6ebbb02
Caleb Brendel <109250665+CalebBrendel@users.noreply.github.com>
2025-05-19 15:04:44 -0500

    Update windows_p2.bat

--- scripts/windows_p2.bat
+++ scripts/windows_p2.bat
@@ -1,29 +1,5 @@
 @echo off
 
-REM Creating directory to store POS Journal Logger...
-
-mkdir C:\pos-journal-logger
-mkdir C:\pos-journal-logger\logs
-mkdir C:\pos-journal-logger\scripts
-
-REM Installing Chocolately if not installed...
-
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
-REM Installing curl if not installed...
-
-choco install curl --force -y
-
-REM Downloading Python 3.13.3...
-
-curl -o "C:\python.exe" "https://www.python.org/ftp/python/3.13.3/python-3.13.3-amd64.exe"
-
-REM Installing Python 3.13.3...
-
-C:\python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-
-REM Will setup another bat script to be called that will excute the rest of this...
-
 REM Downloading the POS Journal Logger Python Script...
 
 curl -o "C:\pos-journal-logger\scripts\pos-journal-logger.py" "https://raw.githubusercontent.com/CalebBrendel/pos-journal-logger/refs/heads/main/pos-journal-logger/pos-journal-logger.py"
